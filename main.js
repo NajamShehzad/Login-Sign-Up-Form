55d51a8850eca9f259c9b963642bcb0d42681b9e
NajamShehzad <najambutt195@gmail.com>
2018-04-04 15:38:38 +0500

    Complete Work Just Need To Add Some CSS

--- main.js
+++ main.js
@@ -1,42 +1,24 @@
-// var name = [2,1,3,5,1,5,45,4,5,5,15,1654,654];
+var condition = JSON.parse(localStorage.getItem("condition"));
+var userName = JSON.parse(localStorage.getItem("userName"));
+var password = JSON.parse(localStorage.getItem("password"));
+        
+
+function signIn() {
+    var name1 = document.getElementById("userName").value;
+    var pass = document.getElementById("pass").value;
+    if (condition) {
+        for(var i = 0 ; i < userName.length; i++){
+            if(userName[i] == name1 && password[i] == pass ){
+                alert("Got It Bro");
+            }
+        }
+
+    }
+    else {
+        alert("Please Check Your Usernam/Password");
+        window.location = "signupPage.html";
+
+    }
 
 
-
-
-// localStorage.setItem("nameList",name);
-
-
-// var nameList =  localStorage.getItem("nameList");
-var condition;
-condition = JSON.parse(localStorage.getItem("condition"));
-// if (condition) {
-//     alert("true111");
-// }
-localStorage.setItem("condition", JSON.stringify(true));
-condition = JSON.parse(localStorage.getItem("condition"));
-// if (condition) {
-//     alert("true222");
-// }
-
-var names = [2, 5, 6, 4, 8, 8, "najam shehzad", 98];
-var names1 = [2, 5, 6, 4, 8, 8, "najam butt", 98];
-
-//names[0] = prompt("New member name?");
-localStorage.setItem("names", JSON.stringify(names));
-localStorage.setItem("names", JSON.stringify(names1));
-var storedNames1 = JSON.parse(localStorage.getItem("names12"));
-var userNames = ["najamshehzad","mutlib butt","A.Rehman Butt"];
-localStorage.setItem("userName",JSON.stringify(userNames));
-var getA = JSON.parse(localStorage.getItem("userName"));
-
-// if (typeof (storedNames1) == "object") {
-//     alert("yes");
-// }
-var arr = [2,5,3,4,8,6,8,54]
-var  name11 =   _.sum(arr);
-
-//storedNames1 = JSON.parse(localStorage.getItem("names"));
-
-//...
-var storedNames = JSON.parse(localStorage.getItem("names"));
-// var name1 = JSON.
+}
