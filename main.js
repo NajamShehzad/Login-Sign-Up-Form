0f8f2432f09a97f79b06677a1dc276d2676362cc
NajamShehzad <najambutt195@gmail.com>
2018-04-10 18:13:03 +0500

    Almost There

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ function signIn() {
         for (var i = 0; i < userName.length; i++) {
             if (userName[i] == name1 && password[i] == pass) {
                 alert("Got It Bro");
+                localStorage.setItem("userNumber", JSON.stringify(i));
                 condition1 = false;
+                window.location.href = "mainPage.html"
+                break;
             }
         }
         if (condition1) {
@@ -34,51 +37,69 @@ function signup() {
     var password = JSON.parse(localStorage.getItem("password"));
     var email = JSON.parse(localStorage.getItem("email"));
     var fullName = JSON.parse(localStorage.getItem("fullName"));
-
+    var userToDo = JSON.parse(localStorage.getItem("userToDo"));
+    var userTitle= JSON.parse(localStorage.getItem("userTitle"));
+    var toDoCondition = JSON.parse(localStorage.getItem("toDoCondition"));
     if (condition) {
         var name1 = document.getElementById("userName").value;
         var condition1 = true;
         for (var i = 0; i < userName.length; i++) {
-            if ( name1 == userName[i]){
+            if (name1 == userName[i]) {
                 condition1 = false;
+                break;
             }
         }
-        if(condition1){
-        userName.push(document.getElementById("userName").value);
-        password.push(document.getElementById("pass").value);
-        email.push(document.getElementById("email").value);
-        fullName.push(document.getElementById("fullName").value);
-        localStorage.setItem("userName", JSON.stringify(userName));
-        localStorage.setItem("password", JSON.stringify(password));
-        localStorage.setItem("fullName", JSON.stringify(fullName));
-        localStorage.setItem("email", JSON.stringify(email));
-        check = userName;
-        check2 = fullName;
-        document.getElementById("userName").value = "";
-        document.getElementById("fullName").value = "";
-        document.getElementById("pass").value = "";
-        document.getElementById("email").value = "";
+        if (condition1) {
+            toDoCondition.push(false);
+            userName.push(document.getElementById("userName").value);
+            password.push(document.getElementById("pass").value);
+            email.push(document.getElementById("email").value);
+            fullName.push(document.getElementById("fullName").value);
+            userToDo.push([]);
+            userTitle.push([]);
+            localStorage.setItem("userTitle", JSON.stringify(userTitle));                        
+            localStorage.setItem("toDoCondition", JSON.stringify(toDoCondition));            
+            localStorage.setItem("userToDo", JSON.stringify(userToDo));
+            localStorage.setItem("userName", JSON.stringify(userName));
+            localStorage.setItem("password", JSON.stringify(password));
+            localStorage.setItem("fullName", JSON.stringify(fullName));
+            localStorage.setItem("email", JSON.stringify(email));
+            check = userName;
+            check2 = fullName;
+            document.getElementById("userName").value = "";
+            document.getElementById("fullName").value = "";
+            document.getElementById("pass").value = "";
+            document.getElementById("email").value = "";
 
-        alert("Thank You For Signing Up");
+            alert("Thank You For Signing Up");
         }
-        else{
+        else {
             alert("User Name Is Already Taken");
         }
     }
     else {
+        toDoCondition = [];
         fullName = [];
         password = [];
         email = [];
         userName = [];
+        userToDo = [];
+        userTitle = []
+        userTitle.push([]);
+        localStorage.setItem("userTitle", JSON.stringify(userTitle)); 
         userName.push(document.getElementById("userName").value);
         email.push(document.getElementById("email").value);
         fullName.push(document.getElementById("fullName").value);
         password.push(document.getElementById("pass").value);
+        userToDo.push([]);
+        toDoCondition.push(false);
         localStorage.setItem("userName", JSON.stringify(userName));
         localStorage.setItem("password", JSON.stringify(password));
         localStorage.setItem("fullName", JSON.stringify(fullName));
         localStorage.setItem("email", JSON.stringify(email));
         localStorage.setItem("condition", JSON.stringify(true));
+        localStorage.setItem("userToDo", JSON.stringify(userToDo));
+        localStorage.setItem("toDoCondition", JSON.stringify(toDoCondition));
         condition = JSON.parse(localStorage.getItem("condition"));
         document.getElementById("userName").value = "";
         document.getElementById("fullName").value = "";
